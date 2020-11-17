5eae04579c7fdd4e7a881043e0442f76600fdad3
Muhammad Rifki Adam A <rifki7080@gmail.com>
2020-11-17 09:09:34 +0700

    update cache management

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,6 @@
 const contacts = document.querySelector('.contacts')
 console.log('contacts', contacts)
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var sidenav = document.querySelectorAll('.sidenav');
     var modals = document.querySelectorAll('.modal');
     var fab = document.querySelectorAll('.fixed-action-btn');
@@ -12,28 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const renderContacts = (data, id) => {
     const html = `<li class="collection-item contact avatar" data-id=${id}>
-    Name :<span class="name">${data.name}</span>
-    <p>Phone num : <span class="phone">${data.phone}</span>
+    <img src="${data.avatar}" alt="" class="circle">
+    Name:<span class="name">${data.first_name} ${data.last_name}</span>
+    <p>Email :<span class="email">${data.email}</span>
     </p>
-    <div href="#!" class="secondary-content" data-id=${id} style="text-align:right;">
-        <i class="material-icons modal-trigger" style="cursor: pointer;" href="#edit_contact_modal">edit</i>
-        <i class="material-icons" style="cursor: pointer;">${data.favorite ? 'star':'star_border'}</i>
-        <i class="material-icons" style="cursor: pointer;">delete_outline</i>
-    </div>
+    
 </li>`;
-
     contacts.innerHTML += html
 }
 
 const removeContact = (id) => {
-    const contact = document.querySelector(`.contact[data-id=${id}]`);
+    const contact = document.querySelector(`.contact[data-id=${id}`);
     contact.remove();
 }
-
-const updateContact = (data,id)=>{
-    const contact = document.querySelector(`.contact[data-id=${id}]`);
+const updateContact = (data, id) => {
+    const contact = document.querySelector(`.contact[data-id="${id}"`);
     contact.querySelector('.name').innerHTML = data.name;
     contact.querySelector('.phone').innerHTML = data.phone;
-    contact.querySelectorAll('.material-icons')[1].textContent = 
-    data.favorite ? 'star' : 'star_border';
+    contact.querySelectorAll('.material-icons')[1].textContent = data.favorite ? 'star' : 'star_border';
+
+
 }
--- sw.js
+++ sw.js
@@ -5,50 +5,142 @@ const assets = [
     '/index.html',
     '/js/app.js',
     '/js/ui.js',
+    '/js/users.js',
     '/js/materialize.min.js',
     '/css/materialize.min.css',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
     '/pages/fallback.html'
 ]
+const limitCacheSize = (name, size) => {
+    console.log('limit cache size')
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            console.log(keys)
+            if (keys.length > size) {
+                console.log('delete')
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
 
-const limitNumberCache = (cacheName, num) => {
-        caches.open(cacheName).then(cache => {
-            cache.keys().then(keys => {
-                if (keys.length > num) {
-                    cache.delete(keys[0]).then(limitNumberCache(cacheName, num))
-                }
-            })
+            }
         })
-    }
-    //install process 
+    })
+}
+const limitNumCache = (cacheName, num) => {
+    caches.open(cacheName).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > num) {
+                cache.delete(keys[0]).then(limitNumCache(cacheName, num));
+
+            }
+        })
+    })
+}
+// const limitNumCache = (cacheName, num) => {
+//     caches.open(cacheName).then(cache => {
+//         cache.keys().then(keys => {
+//             if (keys.length > num) {
+//                 cache.delete(keys[0]).then(limitNumCache(cacheName, num));
+//             }
+//         })
+//     })
+// }
+//install processs
+
 self.addEventListener('install', e => {
-    // console.log('sw is installed') 
     e.waitUntil(
         caches.open(staticCache).then(cache => {
             cache.addAll(assets)
         })
+
     )
+
 })
 
-//activate process
+//activate
 self.addEventListener('activate', e => {
-    console.log('sw is activated')
+    console.log('avtivate')
 })
 
 self.addEventListener('fetch', e => {
-    // console.log('sw fetch event', e)
-    if (e.request.url.indexOf('firestore.googleapis.com') === -1) {
-        e.respondWith(
-            caches.match(e.request).then(staticRes => {
-                return staticRes || fetch(e.request).then(dynamicRes => {
-                    return caches.open(dynamicCache).then(cache => {
-                        cache.put(e.request.url, dynamicRes.clone())
-                        limitNumberCache(dynamicCache, 2)
-                        return dynamicRes
-                    })
+    e.respondWith(
+        caches.match(e.request).then(staticRes => {
+            return staticRes || fetch(e.request).then(dynamicRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(e.request.url, dynamicRes.clone())
+                    return dynamicRes
                 })
-            }).catch(() => caches.match('/pages/fallback.html'))
-        )
-    }
+            })
+        }).catch(() => caches.match('/pages/fallback.html'))
+    )
+
 
 })
+
+// self.addEventListener('fetch', e => {
+//     console.log('fetch events', e)
+//     if (e.request.url.indexOf('firestore.googleapis.com') === -1) {
+//         //if not making the request from google, then can do caching
+//         e.respondWith(
+//             caches.match(e.request).then(cacheRes => {
+//                 return cacheRes || fetch(e.request).then(fetchRes => {
+//                     return caches.open(dynamicCache).then(cache => {
+//                         cache.put(e.request.url, fetchRes.clone());
+//                         limitCacheSize(dynamicCache, 3)
+//                         return fetchRes
+//                     })
+//                 })
+//             }).catch(() => {
+//                 if (e.request.url.indexOf('.html') > -1) {
+//                     return caches.match('/pages/fallback.html');
+
+//                 }
+
+//             })
+//         )
+//     }
+
+// })
+
+// network first
+// self.addEventListener('fetch', e => {
+
+//     e.respondWith(
+//         fetch(e.request).then(fetchRes => {
+//             return caches.open(dynamicCache).then(cache => {
+//                 cache.put(e.request.url, fetchRes.clone());
+//                 return fetchRes
+//             })
+//         }).catch(function () {
+//             return caches.match(e.request);
+//         })
+//     )
+
+
+// })
+// self.addEventListener('fetch', evt => {
+//     evt.respondWith(
+//         fetch(evt.request).then(fetchRes => {
+//             return caches.open(dynamicCacheName).then(cache => {
+//                 cache.put(evt.request.url, fetchRes.clone());
+//                 return fetchRes
+//             })
+//         })
+//             .catch(function () {
+//                 console.log(evt.request, 'match', caches.match(evt.request));
+//                 return caches.match(evt.request);
+//             })
+//     )
+
+
+// })
+//cache only
+// self.addEventListener('fetch', e => {
+
+//     e.respondWith(
+//         caches.match(e.request)
+//     )
+
+
+// })
+
+
